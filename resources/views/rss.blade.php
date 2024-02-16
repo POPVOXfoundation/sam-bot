c2eca82b87f5c690d30cac39d446620e3c1463d5
bryandease <bryan@bryandease.com>
2024-02-16 17:36:42 -0600

    Updated nodes in rss feed

--- resources/views/rss.blade.php
+++ resources/views/rss.blade.php
@@ -2,6 +2,7 @@
 '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
 ?>
 <rss version="2.0"
+     xmlns:sam="https://www.govtdata.org/sam_bot/ns/"
      xmlns:content="http://purl.org/rss/1.0/modules/content/"
      xmlns:dc="http://purl.org/dc/elements/1.1/"
 >
@@ -10,15 +11,15 @@
         <link><![CDATA[ https://www.govtdata.org/sam_bot ]]></link>
         <description><![CDATA[ Curated list from Sam.gov ]]></description>
         <dc:language>en</dc:language>
-        <dc:date>{{ now()->toDateTimeString() }}</dc:date>
+        <pubDate>{{ now()->toRfc7231String() }}</pubDate>
         @foreach($datapoints as $opp)
         <item>
             <dc:identifier><![CDATA[{{ $opp['id'] }}]]></dc:identifier>
             <title><![CDATA[{{ $opp['name'] }}]]></title>
             <link><![CDATA[{{ $opp['websiteUrl'] }}]]></link>
             <dc:date><![CDATA[{{ $opp['publishDate'] }}]]></dc:date>
-            <content:solicitationNumber><![CDATA[{{ $opp['solicitationNumber'] }}]]></content:solicitationNumber>
-            <content:organization><![CDATA[{{ $opp['org'] }}]]></content:organization>
+            <sam:solicitationNumber><![CDATA[{{ $opp['solicitationNumber'] }}]]></sam:solicitationNumber>
+            <sam:organization><![CDATA[{{ $opp['org'] }}]]></sam:organization>
         </item>
         @endforeach
     </channel>
