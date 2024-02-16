8c808739ecbc986f722cfa74d3b8dd2fadc828aa
bryandease <bryan@bryandease.com>
2024-02-16 17:09:54 -0600

    Updated nodes in rss feed

--- resources/views/rss.blade.php
+++ resources/views/rss.blade.php
@@ -1,22 +1,25 @@
 <?=
 '<?xml version="1.0" encoding="UTF-8"?>'.PHP_EOL
 ?>
-<rss version="2.0">
+<rss version="2.0"
+     xmlns:content="http://purl.org/rss/1.0/modules/content/"
+     xmlns:dc="http://purl.org/dc/elements/1.1/"
+>
     <channel>
         <title><![CDATA[ POPVOX Foundation Sam Bot ]]></title>
         <link><![CDATA[ https://www.govtdata.org/sam_bot ]]></link>
         <description><![CDATA[ Curated list from Sam.gov ]]></description>
-        <language>en</language>
-        <pubDate>{{ now() }}</pubDate>
+        <dc:language>en</dc:language>
+        <pubDate>{{ now()->toDateTimeString() }}</pubDate>
 
         @foreach($datapoints as $opp)
             <item>
+                <dc:identifier><![CDATA[{{ $opp['id'] }}]]></dc:identifier>
                 <title><![CDATA[{{ $opp['name'] }}]]></title>
-                <url>{{ $opp['websiteUrl'] }}</url>
-                <publishedDate><![CDATA[{{ $opp['publishDate'] }}]]></publishedDate>
-                <solicitationNumber><![CDATA[{{ $opp['solicitationNumber'] }}]]></solicitationNumber>
-                <organization><![CDATA[{{ $opp['org'] }}]]></organization>
-                <id><![CDATA[{{ $opp['id'] }}]]></id>
+                <link><![CDATA[{{ $opp['websiteUrl'] }}]]></link>
+                <dc:date><![CDATA[{{ $opp['publishDate'] }}]]></dc:date>
+                <content:solicitationNumber><![CDATA[{{ $opp['solicitationNumber'] }}]]></content:solicitationNumber>
+                <content:organization><![CDATA[{{ $opp['org'] }}]]></content:organization>
             </item>
         @endforeach
     </channel>
